47025eb1ed6d66f26693478dcd09f9c4f8a95b87
Xenron2004 <kumarkoushal787075@gmail.com>
2025-05-18 06:30:06 +0530

    nsdf

--- src/components/Bg.jsx
+++ src/components/Bg.jsx
@@ -4,7 +4,7 @@ import Home from "./Home";
 
 const Bg = () => {
   return (
-    <div className="flex max-w-[1280px] mt-4 border-t border-dotted border-gray-300 pt-2 w-11/12 items-center justify-between text-[#212B36] text-[12px] font-sans font-regular ">
+    <div className="flex max-w-[1280px] mt-4 border-t border-dotted border-gray-300 pt-2 w-11/12 items-center justify-between text-[#212B36] text-[14px] font-sans font-regular ">
       <div>
         Designed and developed by{" "}
         <a
